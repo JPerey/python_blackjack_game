de8b78835cf6692111c6a926f19c881a114328b7
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-09-13 21:55:08 -0700

    WIP: added palyer hand scoring

--- blackjackgame.py
+++ blackjackgame.py
@@ -2,16 +2,18 @@ import random
 
 
 # Initialized variables
-deck = []
-player_hand = []
-dealer_hand = []
-player_score = 0
-dealer_score = 0
-stay_hit_correct = False
-stay_hit = ""
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 overall_score = []
 
 # functions
+
+
+def check_double_aces(hand):
+    if hand == [11, 11]:
+        hand[0] = 1
+        hand[1] = 1
+
+    return hand
 
 
 def play():
@@ -28,9 +30,56 @@ def play():
         print("Goodbye.")
     elif play == "yes":
         print("time to play the game.")
-        # bj_game()
-
-# functions
+        bj_game()
 
 
+def bj_game():
+    stay_hit_correct = False
+    player_score = 0
+    dealer_score = 0
+    player_hand = []
+    dealer_hand = []
+
+    for run in range(0, 2):
+        player_hand.append(random.choice(cards))
+        dealer_hand.append(random.choice(cards))
+
+    player_hand = check_double_aces(player_hand)
+    dealer_hand = check_double_aces(dealer_hand)
+
+    player_score = sum(player_hand)
+    dealer_score = sum(dealer_hand)
+    print(f"dealer hand: {dealer_hand} score: {dealer_score}")
+
+    # players hand section
+
+    while sum(player_hand) <= 21:
+        while stay_hit_correct != True:
+            print(f"Player hand: {player_hand} score: {player_score}")
+            stay_hit = input("Would you like to stay or hit? ").lower()
+            if stay_hit == "stay" or stay_hit == "hit":
+                stay_hit_correct = True
+            else:
+                print("invalid input. only input stay or hit.")
+        if stay_hit == "hit":
+            player_hand.append(random.choice(cards))
+            stay_hit_correct = False
+            if sum(player_hand) > 21:
+                for i in range(len(player_hand)):
+                    if player_hand[i] == 11:
+                        player_hand[i] = 1
+            player_score = sum(player_hand)
+        elif stay_hit == "stay":
+            break
+
+    print(f"final player's hand: {player_hand} score: {player_score}")
+    print("Dealer's turn")
+
+    # dealers section - automated
+
+    while sum(dealer_hand) <= 21:
+        if dealer_score < player_score
+
+
+# Timeline
 play()
